3b7552474c9945e5f5b8c11821042d30854b17cf
იაკობ მიროტაძე <42580680+JacobMirotadze@users.noreply.github.com>
2021-11-20 14:23:53 +0400

    new lecture code

--- my-element.js
+++ my-element.js
@@ -30,6 +30,10 @@ export class MyElement extends LitElement {
         background: blue;
       }
       
+      .snake:last-child {
+        border-radius: 20px;
+      }
+      
       .arrows {
         display: flex;
         justify-content: center;
@@ -84,11 +88,14 @@ export class MyElement extends LitElement {
 
   constructor() {
     super();
+    this.lastMove = 'right'
     this.gridArray = [];
     this.fillGrid();
     this.snakeArray = [];
     this.fillSnake();
-    this.headCoordinates = {row: MyElement.snakeLength - 1, column: MyElement.startPositionColumn};
+    this.headCoordinates = {row: MyElement.snakeLength - 1, column: 0};
+    //eventListener
+    //math.random
   }
 
   fillSnake() {
@@ -110,31 +117,68 @@ export class MyElement extends LitElement {
   }
 
   moveRight() {
-    this.snakeArray.splice(0,1);
-    this.headCoordinates = {row: this.headCoordinates.row +1, column: this.headCoordinates.column}
-    this.snakeArray.push(this.headCoordinates.row +':'+ this.headCoordinates.column)
-    this.snakeArray = [...this.snakeArray]
+    if(this.lastMove !== 'left') {
+      this.move(1, 0)
+      setTimeout(() => {
+        if (this.lastMove === 'right') this.moveRight()
+      }, 1000)
+    }
   }
 
   moveLeft() {
-    this.snakeArray.splice(0,1);
-    this.headCoordinates = {row: this.headCoordinates.row -1, column: this.headCoordinates.column}
-    this.snakeArray.push(this.headCoordinates.row +':'+ this.headCoordinates.column)
-    this.snakeArray = [...this.snakeArray]
+    if(this.lastMove !== 'right') {
+      this.lastMove = 'left'
+      this.move(-1, 0)
+      setTimeout(() => {
+        if (this.lastMove === 'left') this.moveLeft()
+      }, 1000)
+    }
   }
-
   moveTop() {
-    this.snakeArray.splice(0,1);
-    this.headCoordinates = {row: this.headCoordinates.row, column: this.headCoordinates.column -1}
-    this.snakeArray.push(this.headCoordinates.row +':'+ this.headCoordinates.column)
-    this.snakeArray = [...this.snakeArray]
+    if(this.lastMove !== 'bottom'){
+      this.lastMove = 'top';
+      this.move(0, -1)
+
+      setTimeout(() => {
+        if (this.lastMove === 'top') this.moveTop()
+      }, 1000)
+      }
   }
 
   moveBottom() {
+    if(this.lastMove !== 'top') {
+      this.lastMove = 'bottom'
+      this.move(0, 1)
+      setTimeout(() => {
+        if (this.lastMove === 'bottom') this.moveBottom()
+      }, 1000)
+    }
+  }
+
+  move(additionalRow, additionalColumn) {
+    this.headCoordinates.row += additionalRow
+    this.headCoordinates.column += additionalColumn
+    this.checkBorders();
+    if(!this.lastMove) return;
     this.snakeArray.splice(0,1);
-    this.headCoordinates = {row: this.headCoordinates.row, column: this.headCoordinates.column +1}
     this.snakeArray.push(this.headCoordinates.row +':'+ this.headCoordinates.column)
-    this.snakeArray = [...this.snakeArray]
+    this.snakeArray = [...this.snakeArray];
+
+  }
+
+  checkBorders() {
+    if(
+      this.headCoordinates.row >= MyElement.rowLength ||
+      this.headCoordinates.column >= MyElement.rowLength ||
+      this.headCoordinates.row < 0 ||
+      this.headCoordinates.column < 0
+    ){
+      this.lastMove = '';
+    }
+  }
+
+  eatApple(){
+    
   }
 
 }
